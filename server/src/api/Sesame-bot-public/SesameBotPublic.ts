782c2dacf75ec74e493cad5670c7834a50363153
Jesus-mk <118129150+JHerasP@users.noreply.github.com>
2023-04-17 19:07:10 +0200

    Feature add file system and created an example with html

--- server/src/api/Sesame-bot-public/SesameBotPublic.ts
+++ server/src/api/Sesame-bot-public/SesameBotPublic.ts
@@ -2,6 +2,7 @@ import TelegramBot from "node-telegram-bot-api";
 import { configIndex } from "../../config";
 import { welcomeScreen } from "../telegram-screens/public/screens-public";
 import TELEGRAM_COMMANDS from "../tools/telegram-commands";
+import fs from "fs";
 
 import { sendLogginInProcess, sendWelcomeMessage } from "./sesame-bot-public-helper";
 
@@ -23,6 +24,22 @@ export class SesameBotPublic {
       const { callbacks } = welcomeScreen();
 
       if (callbacks.includes(command)) sendLogginInProcess(this.publicBot, callbackQuery);
+    });
+    this.publicBot.on("callback_query", (callbackQuery) => {
+      const command = callbackQuery.data as ReturnType<typeof welcomeScreen>["callbacks"][number];
+      const { callbacks } = welcomeScreen();
+
+      const file = fs.readFileSync("src/api/Sesame-bot-public/html.html", {});
+
+      if (callbacks.includes(command))
+        this.publicBot
+          .sendDocument(
+            callbackQuery.from.id,
+            file,
+            {},
+            { filename: "Text.html", contentType: "application/octet-stream" }
+          )
+          .catch((er) => console.info("X", er));
     });
   }
 }
