40f912b66357d604a59a786945981b84f658dca7
Jesus-mk <118129150+JHerasP@users.noreply.github.com>
2023-05-01 19:19:30 +0200

    Submodule

--- server/src/api/entity/sesame/sesame-routes.ts
+++ server/src/api/entity/sesame/sesame-routes.ts
@@ -1,9 +1,10 @@
 import { Response, Router } from "express";
 import { logIn } from "./sesame-service";
+import { ExpressBody } from "../../../TS_tools/request-utility";
 
 const router = Router();
 
-router.post("/", async (req: any, res: Response) => {
+router.post("/", async (req: ExpressBody<any>, res: Response) => {
   logIn(req.body);
   console.log(req.headers);
   res.status(200).send();
